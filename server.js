67b70283dc961ad1125012a75ae20128cd562d06
Colson <colson@Colsons-MacBook-Pro.local>
2022-04-06 07:54:31 +0545

    connected to mongodb cloud

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+  console.log("connected to mongodb");
+});
 
 const app = express();
 
